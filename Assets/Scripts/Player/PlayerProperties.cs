7eabef1bcf95bbd7ddd54310aeee2aac2cafafeb
Matúš <balazi@stud.uniza.sk>
2023-04-04 12:35:12 +0200

    Fixed color fading

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -7,10 +7,12 @@ public static class PlayerProperties
     public static float gravityForce = 100f;
     public static Color playerColor;
     public static bool isStickActive = false;
-    public static float remainingStickTime = 8f;
+    public static float remainingStickTime = 80f;
     public static float timeUntilStickRegen = 2f;
     public static float remainingColorTime = 10f;
     public static float timeUntilColorReset = 10f;
     public static int playerLifes = 3;
     public static int playerGems = 0;
+    public static float colorChangeCooldown = 10f;
+    public static float colorChangeCountdown = 10f;
 }
