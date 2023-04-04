c548c11b1bf02827d75d4ab35dc271fa09f32809
Matúš <balazi@stud.uniza.sk>
2023-04-04 21:45:00 +0200

    Fixed PlatformColor script and other stuff

--- Assets/Scripts/Platform/PlatformColor.cs
+++ Assets/Scripts/Platform/PlatformColor.cs
@@ -3,23 +3,55 @@ using UnityEngine;
 public class PlatformColor : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] Color playerColor;
     [SerializeField] Color platformColor;
+    [SerializeField] Color currentPlayerColor;
     private new SpriteRenderer renderer;
 
     private void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
         renderer.color = Colors.GetRandomColor();
+        playerColor = player.GetComponent<SpriteRenderer>().color;
         platformColor = gameObject.GetComponent<SpriteRenderer>().color;
+        currentPlayerColor = PlayerProperties.playerColor;
+    }
+
+    private void Update()
+    {
+        if (currentPlayerColor != PlayerProperties.playerColor)
+        {
+            currentPlayerColor = PlayerProperties.playerColor;
+
+            if (PlayerProperties.playerColor.Equals(platformColor) && !PlayerProperties.playerColor.Equals(playerColor))
+            {
+                this.gameObject.GetComponent<Collider2D>().isTrigger = false;
+            } 
+            else
+            {
+                this.gameObject.GetComponent<Collider2D>().isTrigger = true;          
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (PlayerProperties.playerColor.Equals(platformColor))
-            collision.gameObject.GetComponent<Collider2D>().isTrigger = false;
+        if (collision.gameObject.CompareTag("Player") && PlayerProperties.playerColor.Equals(platformColor) && !PlayerProperties.playerColor.Equals(playerColor))
+            this.gameObject.GetComponent<Collider2D>().isTrigger = false;
         else
         {
-            collision.gameObject.GetComponent<Collider2D>().isTrigger = true;
+            this.gameObject.GetComponent<Collider2D>().isTrigger = true;
+            //player.transform.SetParent(null); //TODO PRECO???
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && PlayerProperties.playerColor.Equals(platformColor) && !PlayerProperties.playerColor.Equals(playerColor))
+            this.gameObject.GetComponent<Collider2D>().isTrigger = false;
+        else
+        {
+            this.gameObject.GetComponent<Collider2D>().isTrigger = true;
             //player.transform.SetParent(null); //TODO PRECO???
         }
     }
