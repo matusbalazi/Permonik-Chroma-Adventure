ed4839fb6cad69a5f775f0e3bba4563c1497714d
Matúš <balazi@stud.uniza.sk>
2023-04-04 11:38:57 +0200

    Upgraded stick and jump

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public static class PlayerProperties
 {
-    public static float speedForce = 120f;
-    public static float jumpForce = 50f;
-    public static float gravityForce = 300f;
+    public static float speedForce = 80f;
+    public static float jumpForce = 100f;
+    public static float gravityForce = 100f;
     public static Color playerColor;
     public static bool isStickActive = false;
-    public static float remainingStickTime = 80f;
+    public static float remainingStickTime = 8f;
     public static float timeUntilStickRegen = 2f;
     public static float remainingColorTime = 10f;
     public static float timeUntilColorReset = 10f;
